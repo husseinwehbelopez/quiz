a41d11220adb765f2a940e949eccbbe3d58b41c6
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-18 19:08:57 +0200

    Creditos?

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+app.use('/author', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
