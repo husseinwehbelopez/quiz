86aefcdb8fc03e490e797f9ecc0721a93cf993c9
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-23 00:26:52 +0200

    Validar preg resp

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.use(function(err, req, res, next) {
 res.status(err.status || 500);
 res.render('error', {
 message: err.message,
-error: err
+error: err,
+errors: []
 });
 });
 }
@@ -44,10 +45,10 @@ app.use(function(err, req, res, next) {
 res.status(err.status || 500);
 res.render('error', {
 message: err.message,
-error: {}
+error: {},
+errors: []
 });
 });
-
 
 
 module.exports = app;
