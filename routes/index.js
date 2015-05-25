5ea1e62819d1008c427736a52d3b295c2fc9c98d
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-25 15:54:17 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ res.render('index', { title: 'Quiz', errors: []});
 });
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 // Definición de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
 router.post('/login', sessionController.create); // crear sesión
@@ -25,6 +26,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+sessionController.loginRequired, commentController.publish);
 
 
 module.exports = router;
