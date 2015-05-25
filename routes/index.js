46146ca2058582043d291ef181ce6e192c2d713c
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-25 12:15:35 +0200

    Autenticación de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 // Página de entrada (home page)
 router.get('/', function(req, res) {
 res.render('index', { title: 'Quiz', errors: []});
 });
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+// Definición de rutas de sesion
+router.get('/login', sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
@@ -17,6 +22,8 @@ router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
 module.exports = router;
