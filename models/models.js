601b08b5a3ab5488bc8dc42c0771cc674711c328
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-25 11:15:35 +0200

    Crear Comentarios

--- models/models.js
+++ models/models.js
@@ -25,19 +25,23 @@ omitNull: true // solo Postgres
 // Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 exports.Quiz = Quiz; // exportar tabla Quiz
+exports.Comment = Comment;
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
 // then(..) ejecuta el manejador una vez creada la tabla
-Quiz.count().then(function(count){
-if(count === 0){
-Quiz.create({ pregunta: 'Capital de Italia',
-respuesta: 'Roma'
-});
-Quiz.create({ pregunta: 'Capital de Portugal',
-respuesta: 'Lisboa'
-})
-.then(function(){console.log('Base de datos inicializada')});
+Quiz.count().then(function (count){
+if(count === 0) { // la tabla se inicializa solo si está vacía
+Quiz.bulkCreate(
+[ {pregunta: 'Capital de Italia', respuesta: 'Roma'},
+{pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+]
+).then(function(){console.log('Base de datos inicializada')});
 };
 });
 
