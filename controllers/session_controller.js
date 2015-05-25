7bea36e2e1466f2b6f667c33f0db935846144058
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-25 12:30:54 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,11 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+if (req.session.user) {
+next();
+} else {
+res.redirect('/login');
+}
+};
 // Get /login -- Formulario de login
 exports.new = function(req, res) {
 var errors = req.session.errors || {};
@@ -25,4 +33,5 @@ res.redirect(req.session.redir.toString());// redirección a path anterior a log
 exports.destroy = function(req, res) {
 delete req.session.user;
 res.redirect(req.session.redir.toString()); // redirect a path anterior a login
+
 };
