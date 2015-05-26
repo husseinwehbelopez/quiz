238cd2ff96a1cba53bf4595294a296d0cc44baf7
husseinwehbelopez <hussein.wehbe.lopez@alumnos.upm.es>
2015-05-26 12:54:02 +0200

    Imagenes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -63,6 +63,9 @@ res.render('quizes/new', {quiz: quiz, errors: []});
 // POST /quizes/create
 exports.create = function(req, res) {
 req.body.quiz.UserId = req.session.user.id;
+if(req.files.image){
+req.body.quiz.image = req.files.image.name;
+}
 var quiz = models.Quiz.build( req.body.quiz );
 quiz
 .validate()
@@ -72,7 +75,7 @@ if (err) {
 res.render('quizes/new', {quiz: quiz, errors: err.errors});
 } else {
 quiz // save: guarda en DB campos pregunta y respuesta de quiz
-.save({fields: ["pregunta", "respuesta", "UserId"]})
+.save({fields: ["pregunta", "respuesta", "UserId", "image"]})
 .then( function(){ res.redirect('/quizes')})
 } // res.redirect: Redirección HTTP a lista de preguntas
 }
@@ -85,6 +88,9 @@ res.render('quizes/edit', {quiz: quiz, errors: []});
 };
 // PUT /quizes/:id
 exports.update = function(req, res) {
+if(req.files.image){
+req.quiz.image = req.files.image.name;
+}
 req.quiz.pregunta = req.body.quiz.pregunta;
 req.quiz.respuesta = req.body.quiz.respuesta;
 req.quiz
@@ -95,7 +101,7 @@ if (err) {
 res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 } else {
 req.quiz // save: guarda campos pregunta y respuesta en DB
-.save( {fields: ["pregunta", "respuesta"]})
+.save( {fields: ["pregunta", "respuesta", "image"]})
 .then( function(){ res.redirect('/quizes');});
 } // Redirección HTTP a lista de preguntas (URL relativo)
 }
--- models/quiz.js
+++ models/quiz.js
@@ -1,3 +1,4 @@
+
 // Definicion del modelo de Quiz con validación
 module.exports = function(sequelize, DataTypes) {
 return sequelize.define(
@@ -9,6 +10,9 @@ validate: { notEmpty: {msg: "-> Falta Pregunta"}}
 respuesta: {
 type: DataTypes.STRING,
 validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+},
+image: {
+type: DataTypes.STRING
 }
 }
 );
